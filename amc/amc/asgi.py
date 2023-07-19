dcd5826f619bc8758440ef8943dacad0952a94c4
mattfussell <matt.fussell@gmail.com>
2023-07-18 20:50:50 -0500

    blew everything up - starting over
    Trying to remember everything from the beginning is hard.

--- amc/amc/asgi.py
+++ amc/amc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for army_men project.
+ASGI config for amc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'army_men.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amc.settings')
 
 application = get_asgi_application()
--- amc/amc/urls.py
+++ amc/amc/urls.py
@@ -1,4 +1,4 @@
-"""army_men URL Configuration
+"""amc URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
